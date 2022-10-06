c4c5700d5e98928388f98e3d3c828783ffabaf2e
MaksDubina <mgd8832@gmail.com>
2022-10-06 17:37:39 +0700

    add hw4 fixed error

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,15 +18,19 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
+        + (disabled
+            ? ' ' + s.disabled
+            : xType === 'red'
+                ? ' ' + s.red
+                : xType === 'secondary'
+                    ? ' ' + s.secondary
+                    : ' ')
 
-        + (xType === 'red'? ' ' + s.red: '')
-        + (xType === 'secondary'? ' ' + s.secondary: '')
-        + (disabled? ' ' + s.disabled: '')
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+
+    // + (xType === 'red'? ' ' + s.red: '')
+    // + (xType === 'secondary'? ' ' + s.secondary: '')
+    // + (disabled? ' ' + s.disabled: '')
+    // + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
         <button
