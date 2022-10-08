036c50c084f80fee0d6c853108d971cb3d4e83e9
MaksDubina <mgd8832@gmail.com>
2022-10-08 18:27:22 +0700

    delete div wrapper

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -21,7 +21,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
             {open && <div className={s.background} onClick={handleClose}/>}
 
             <aside className={sidebarClass}>
-                {/*<div className={s.nav_wrapper}>*/}
+                <div className={s.nav_wrapper}>
                     <button className={s.close} onClick={handleClose}>
                         <img
                             src={closeIcon}
@@ -35,7 +35,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                             id={'hw5-pre-junior-link'}
                             to={PATH.PRE_JUNIOR}
                             onClick={handleClose}
-                             className={location.pathname === '/pre-junior' ? `active`: ''} // делает студент
+                             //className={location.pathname === '/pre-junior' ? `${s.active} active`: ''} // делает студент
+                            className={({isActive})=> isActive? s.active :''}
                         >
                             Pre-junior
                         </NavLink>
@@ -43,7 +44,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                             id={'hw5-junior-link'}
                             to={PATH.JUNIOR}
                             onClick={handleClose}
-                            className={location.pathname === '/junior' ? `active`: ''} // делает студент
+                            //className={location.pathname === '/junior' ? `${s.active} active`: ''} // делает студент
+                            className={({isActive})=> isActive? s.active :''}
                         >
                             Junior
                         </NavLink>
@@ -51,12 +53,13 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                             id={'hw5-junior-plus-link'}
                             to={PATH.JUNIOR_PLUS}
                             onClick={handleClose}
-                            className={location.pathname === '/junior-plus' ? `active`: ''} // делает студент
+                            //className={location.pathname === '/junior-plus' ? `${s.active} active`: ''} // делает студент
+                            className={({isActive})=> isActive? s.active :''}
                         >
                             Junior Plus
                         </NavLink>
                     </nav>
-                {/*</div>*/}
+                </div>
             </aside>
         </>
     )
