3f87fd2ef961f04feece9fb415b9f897b549e0bb
MaksDubina <mgd8832@gmail.com>
2022-11-03 16:32:31 +0700

    add hw12

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import {Slider, SliderProps, SliderThumb} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
+
+
+
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                minWidth: '147px',
+                 color: '#00CC22;',
+                '& .MuiSlider-thumb': {
+                    backgroundColor: '#fff',
+                    border: '2px solid currentColor',
+                },
+                '& .airbnb-bar': {
+                    height: 9,
+                    width: 1,
+                    backgroundColor: 'currentColor',
+                    marginLeft: 1,
+                    marginRight: 1,
+                },
+
             }}
+
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,22 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: changeThemeIdType): stateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {...state, themeId:action.id}
         default:
             return state
     }
 }
+type changeThemeIdType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type stateType = {
+    themeId: number
+}
+
+export const changeThemeId = (id: number): changeThemeIdType => ({ type: 'SET_THEME_ID', id }) // fix any
